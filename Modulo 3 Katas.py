e4f4a34bdbe86ec3d82ef45ebaa3e02d2b003d26
JavierChaparro <javierchm21@gmail.com>
2022-02-19 01:11:29 -0600

    add: Notebook

---
+++ Modulo 3 Katas.py
@@ -0,0 +1,33 @@
+
+
+# Añadir el código necesario para crear una variable que guarde la velocidad del asteroide.
+v = 49
+a = 25
+# Escribe una expresión de prueba para calcular si necesita una advertencia.
+if v >= a : # Agregue las instrucciones que se ejecutarán si la expresión de prueba es true o false.
+    print ("Advertencia")
+
+
+v=19 # Agrega el código para crear una variable para un asteroide que viaja a 19 km/s
+
+if v >= 20: # Escribe varias expresiones de prueba para determinar si puedes ver el rayo de luz desde la tierra
+    print ("El asteroide sera visible desde la tierra") # Agrega las instrucciones que se ejecutarán si las expresiones de prueba son True o False
+else: 
+    print("El asteroide no sera visible desde la tierra")
+
+
+
+velocidad_asteroide= 25
+tamano_asteroide= 40
+    # Agrega el código para crear nuevas variables para la velocidad y el tamaño del asteroide
+
+    # Para probar el código, prueba con varias velocidades y tamaños
+if velocidad_asteroide >= 25 and tamano_asteroide >= 25:
+    print("¡Alerta, un asteroide muy peligoso viene a la tierra!")
+elif velocidad_asteroide >= 20:
+    print("lookup! ¡Hay una luz magica en el cielo!")
+elif tamano_asteroide  < 25 :
+    print("Nada que ver aqui")
+else:
+    print("nada que ver aqui")
+   # Escribe varias expresiones de prueba o combinaciones de expresiones de prueba para determinar qué mensaje se debe enviar a Ti
